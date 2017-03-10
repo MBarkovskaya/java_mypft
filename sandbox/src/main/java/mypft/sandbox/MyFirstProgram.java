f06b1756b34d64a1df32a6796c439e5725bffcc1
MBarkovskaya <mariya.barkovskaya@gmail.com>
2017-03-10 20:49:28 +0300

    main класс с функцией, вычисляющей расстояния между точками

--- sandbox/src/main/java/mypft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/mypft/sandbox/MyFirstProgram.java
@@ -4,6 +4,13 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
     System.out.println("Hello, world!");
+    Point p1 = new Point(1, 4);
+    Point p2 = new Point(5, 10);
+    System.out.println("Расстояние между двумя точками p1 и p2" + " = " + distance(p1, p2));
+  }
+  public static double distance(Point p1, Point p2) {
+    double sum = (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
+    return Math.sqrt(sum);
   }
 
 }
