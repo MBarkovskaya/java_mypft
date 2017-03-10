f735da73e656890fd10347c5aa4c057fa4db9854
MBarkovskaya <mariya.barkovskaya@gmail.com>
2017-03-10 22:28:51 +0300

    Создан метод в классе Point и примеры использования в запускаемом классе

--- sandbox/src/main/java/mypft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/mypft/sandbox/MyFirstProgram.java
@@ -4,13 +4,10 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
     System.out.println("Hello, world!");
-    Point p1 = new Point(1, 4);
-    Point p2 = new Point(5, 10);
-    System.out.println("Расстояние между двумя точками p1 и p2" + " = " + distance(p1, p2));
-  }
-  public static double distance(Point p1, Point p2) {
-    double sum = (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
-    return Math.sqrt(sum);
+    Point p = new Point(1, 3);
+    Point other = new Point(5, 10);
+
+    System.out.println("Расстояние между двумя точками с координатами (x1 = " + 1.0 + ", y1 = " + 4.0 + "; x2 = " + 12.0 + ", y2 = " + 5.0 + ") = " + p.distanceTo(other));
   }
 
 }
--- sandbox/src/main/java/mypft/sandbox/Point.java
+++ sandbox/src/main/java/mypft/sandbox/Point.java
@@ -4,10 +4,15 @@ public class Point {
 
   public double x;
   public double y;
+  
 
   public Point(double x, double y) {
     this.x = x;
     this.y = y;
+  }
+  public double distanceTo(Point other) {
+    double sum = (this.x - other.x) * (this.x - other.x) + (this.y - other.y) * (this.y - other.y);
+    return Math.sqrt(sum);
   }
 
 }
