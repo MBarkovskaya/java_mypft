b2e358cf3810e60798e8df297b042fc2cdf11eb2
MBarrkovskaya <mariya,barkovskaya@gmail.com>
2017-03-21 10:56:01 +0300

    созданы тесты удаления контактов из группы RemoveContactFromGroupTests

---
+++ addressbook-web-tests/src/test/java/mypft/addressbook/tests/RemoveContactFromGroupTests.java
@@ -0,0 +1,15 @@
+package mypft.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+public class RemoveContactFromGroupTests extends TestBase {
+
+    @Test
+    public void testRemoveContactFromGroup() {
+        app.getNavigationHelper().gotoHomePage();
+        app.getGroupHelper().selectGroupCombo();
+        app.getContactHelper().selectContact();
+        app.getContactHelper().removeContactFromGroup();
+        app.getContactHelper().returnToSelectedGropePage();
+    }
+}
