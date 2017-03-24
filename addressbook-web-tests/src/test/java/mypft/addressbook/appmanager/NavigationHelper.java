7be2ad207f587bfddfd81af0105dbee7b0b8037f
MBarrkovskaya <mariya,barkovskaya@gmail.com>
2017-03-24 14:42:09 +0300

    Оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/mypft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/mypft/addressbook/appmanager/NavigationHelper.java
@@ -5,19 +5,27 @@ import org.openqa.selenium.WebDriver;
 
 public class NavigationHelper extends BaseHelper {
 
-    public NavigationHelper(WebDriver wd) {
-        super(wd);
-    }
+  public NavigationHelper(WebDriver wd) {
+    super(wd);
+  }
 
-    public void gotoGroupPage() {
-       click(By.linkText("groups"));
+  public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1")) && wd.findElement(By.tagName("h1")).getText().equals("Groups") &&
+            isElementPresent(By.name("new"))) {
+      return;
     }
+    click(By.linkText("groups"));
+  }
 
-    public void gotoContactPage() {
-        click(By.linkText("add new"));
+  public void gotoContactPage() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
     }
-    public void gotoHomePage() {
-        click(By.linkText("home"));
-    }
+    click(By.linkText("add new"));
+  }
+
+  public void gotoHomePage() {
+    click(By.linkText("home"));
+  }
 
 }
