36101473c6f6e7dd39fe2ee2a5477153788261c6
MBarrkovskaya <mariya,barkovskaya@gmail.com>
2017-03-24 14:40:33 +0300

    в параметры ContactData добавлен group

--- addressbook-web-tests/src/test/java/mypft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/mypft/addressbook/model/ContactData.java
@@ -1,46 +1,50 @@
 package mypft.addressbook.model;
 
-import org.openqa.selenium.By;
-
 public class ContactData {
 
-    private final String firstname;
-    private final String lastname;
-    private final String address;
-    private final String home;
-    private final String address2;
-    private final String email;
+  private final String firstname;
+  private final String lastname;
+  private final String address;
+  private final String home;
+  private String group;
+  private final String address2;
+  private final String email;
 
-    public ContactData(String firstname, String lastname, String address, String home, String address2, String email) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.address = address;
-        this.home = home;
-        this.address2 = address2;
-        this.email = email;
-    }
+  public ContactData(String firstname, String lastname, String address, String home, String group, String address2, String email) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.address = address;
+    this.home = home;
+    this.group = group;
+    this.address2 = address2;
+    this.email = email;
+  }
 
-    public String getFrstname() {
-        return firstname;
-    }
+  public String getFrstname() {
+    return firstname;
+  }
 
-    public String getLastname() {
-        return lastname;
-    }
+  public String getLastname() {
+    return lastname;
+  }
 
-    public String getAddress() {
-        return address;
-    }
+  public String getAddress() {
+    return address;
+  }
 
-    public String getHome() {
-        return home;
-    }
+  public String getHome() {
+    return home;
+  }
 
-    public String getAddress2() {
-        return address;
-    }
+  public String getAddress2() {
+    return address;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getEmail() {
+    return email;
+  }
+
+  public String getGroup() {
+    return group;
+  }
 }
