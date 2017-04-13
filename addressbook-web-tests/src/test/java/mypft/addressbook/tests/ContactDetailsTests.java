94d3b1d98a02f13ad7fa351c9235635024392341
MBarkovskaya <mariya.barkovskaya@gmail.com>
2017-04-13 21:38:45 +0300

    Реализован тест ContactDetailsTests для проверки страницы с подробной информацией о контакте

---
+++ addressbook-web-tests/src/test/java/mypft/addressbook/tests/ContactDetailsTests.java
@@ -0,0 +1,52 @@
+package mypft.addressbook.tests;
+
+import mypft.addressbook.model.ContactData;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ContactDetailsTests extends TestBase {
+
+  @BeforeMethod
+  public void ensurePreconditions() {
+    app.goTo().HomePage();
+    app.contact().selectContactAll();
+    if (app.contact().all().size() == 0) {
+      app.contact().create(new ContactData()
+                      .withFirstname("Mariya").withLastname("Barkovskaya").withAddress("Taganrog\nstreet Karantinnaya")
+                      .withHomePhone("9612345").withMobilePhone("+22").withWorkPhone("22-212").withEmail("mariya.barkovskaya@gmail.com"),
+              true);
+    }
+  }
+
+  @Test
+  public void testContactPreview() {
+    ContactData contact = app.contact().all().iterator().next();
+    String contactinfoFromDetailsForm = app.contact().infoFromDetailsForm(contact.getId());
+    assertThat(mergeContact(contact), equalTo(contactinfoFromDetailsForm));
+  }
+
+  private String mergeContact(ContactData contact) {
+    return Stream.of(contact.getFirstname() + "" + contact.getLastname(), multiLineStringToString(contact.getAddress()),
+            multiLineStringToString(contact.getAllPhones()), multiLineStringToString(contact.getAllEmails()))
+            .filter((s) -> !s.equals("")).collect(Collectors.joining(";"));
+  }
+
+  public static String phoneCleaned(String phone) {
+    return phone.replaceAll("^(?:[HMW]): (.*)", "$1").replaceAll("\\s", "").replaceAll("[-()]", "");
+  }
+
+  private static String multiLineStringToString(String multiline) {
+    return Arrays.stream(multiline.split("\n")).filter(s -> !s.equals("")).map(ContactDetailsTests::mainCleaned).collect(Collectors.joining(";"));
+  }
+
+  private static String mainCleaned(String phone) {
+    return phone.replaceAll("[-()]", "").replaceAll("\\s", "");
+  }
+}
