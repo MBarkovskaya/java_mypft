8a90e8156180b5c70e86527a4a9837f6d6b570d8
MBarrkovskaya <mariya,barkovskaya@gmail.com>
2017-05-12 12:15:25 +0300

    уменьшено кол-во тестов в наборе для отладки; реализована возможность удаленного запуска браузера

--- addressbook-web-tests/src/test/java/mypft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/mypft/addressbook/appmanager/ApplicationManager.java
@@ -1,13 +1,17 @@
 package mypft.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -38,13 +42,28 @@ public class ApplicationManager {
     //кроме того имеет смысл сначала инициализировать более быстрые помощники
     dbHelper = new DbHelper();
 
-    if (browser.equals(FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (browser.equals(CHROME)) {
-      wd = new ChromeDriver();
-    } else if (browser.equals(IE)) {
-      wd = new InternetExplorerDriver();
+    //если свойство properties.getProperty selenium.server равно пустой строке, то инициализацию нужно оставить такой же как раньше
+    if ("".equals(properties.getProperty("selenium.server"))) {
+      if (browser.equals(FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (browser.equals(CHROME)) {
+        wd = new ChromeDriver();
+      } else if (browser.equals(IE)) {
+        wd = new InternetExplorerDriver();
+      }
+      //иначе - это не пустая строка, т.е. мы хотим использовать удаленный селениумсервер
+      //и значит нужно использовать другой тип драйвера
+      //в качестве первого параметра нужно передать адрес селениум сервера
+      //второй параметр capabilities - содержит информацию о том, какой драйвер мы хотим использовать
+    } else {
+      //тип должен быть DesiredCapabilities
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      //и устанавливаем там браузер, а селениум сервер будет сам разбираться какой драйвер он должен использовать для того
+      // чтобы запустить конкретный сервер
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
+
     wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd);
